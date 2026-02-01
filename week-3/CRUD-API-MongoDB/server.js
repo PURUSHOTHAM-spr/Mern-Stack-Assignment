b86dfeb6353e357e89d374d13cd5f4597abf3f72
PURUSHOTHAM-spr <purushothamshagamreddy464@gmail.com>
2026-02-02 00:11:03 +0530

    CRUD APIs using local memory and MongoDB

---
+++ week-3/CRUD-API-MongoDB/server.js
@@ -0,0 +1,31 @@
+import exp from "express";
+import { userApp } from "./userApi.js";
+import { productApp } from "./productApi.js";
+import { connect } from "mongoose";
+// create HTTP server
+const app = exp();
+app.use(exp.json());
+
+// connect to db
+const connection = async () => {
+    try {
+let database= await connect("mongodb://127.0.0.1:27017/Anuragdb");
+        console.log("connection success");
+        // Assign port 
+        app.listen(3000, () => console.log("Sever is listening on port 3000"));
+    } catch (err) {
+        console.log("Err in connection", err);
+    }
+}
+connection()
+
+
+// If path starts with /user-api , forward req to userApp
+app.use('/user-api', userApp);
+// If path starts with /product-api , forward req to productApp
+app.use('/product-api', productApp);
+
+// error handling middleware(should always be placed at the end of server or anyfile)
+app.use((err,req,res,next) => {
+    res.status(500).json({message:"error occured",reason:err.message});
+})
